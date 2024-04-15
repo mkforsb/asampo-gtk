9ead827662ed51cbace4404a77da7564c17fbaea
Mikael Forsberg <mkforsb@gmail.com>
2024-04-15 21:57:17 +0200

    utils: skip IDs containing dashes in get-template-children.py

--- utils/get-template-children.py
+++ utils/get-template-children.py
@@ -3,6 +3,9 @@ import re
 data = open("resources/asampo.ui").read()
 
 for x in re.findall(r'class="(.+?)" id="(.+?)"', data):
+    if "-" in x[1]:
+        continue
+
     print("    #[template_child]")
     print(f"    pub {x[1]}: gtk::TemplateChild<gtk::{x[0][3:]}>,")
     print()
