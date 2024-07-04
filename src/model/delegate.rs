2c50b18d433adcc0a625b4b90527e025e6e915dd
Mikael Forsberg <mkforsb@gmail.com>
2024-07-04 11:12:49 +0200

    fix: mistakes in delegation macros

--- src/model/delegate.rs
+++ src/model/delegate.rs
@@ -5,44 +5,44 @@
 #![allow(unused_macros, unused_imports)]
 
 macro_rules! delegate {
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) -> Result) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) -> Result) => {
         pub fn $fname(self $(, $param0: $tp0 $(, $param: $tp)*)?) -> Result<AppModel, anyhow::Error> {
-            Ok(Model {
+            Ok(AppModel {
                 $subm: self.$subm.$fname($($param0 $(, $param)*)?)?,
                 ..self
             })
         }
     };
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) -> Model) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) -> Model) => {
         pub fn $fname(self $(, $param0: $tp0 $(, $param: $tp)*)?) -> AppModel {
-            Model {
+            AppModel {
                 $subm: self.$subm.$fname($($param0 $(, $param)*)?),
                 ..self
             }
         }
     };
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) -> $rt:ty) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) -> $rt:ty) => {
         pub fn $fname(&self $(, $param0: $tp0 $(, $param: $tp)*)?) -> $rt {
             self.$subm.$fname($($param0 $(, $param)*)?)
         }
     };
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) as $name:ident -> Result) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) as $name:ident -> Result) => {
         pub fn $name(self $(, $param0: $tp0 $(, $param: $tp)*)?) -> Result<AppModel, anyhow::Error> {
-            Ok(Model {
+            Ok(AppModel {
                 $subm: self.$subm.$fname($($param0 $(, $param)*)?)?,
                 ..self
             })
         }
     };
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) as $name:ident -> Model) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) as $name:ident -> Model) => {
         pub fn $name(self $(, $param0: $tp0 $(, $param: $tp)*)?) -> AppModel {
-            Model {
+            AppModel {
                 $subm: self.$subm.$fname($($param0 $(, $param)*)?),
                 ..self
             }
         }
     };
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) as $name:ident -> $rt:ty) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) as $name:ident -> $rt:ty) => {
         pub fn $name(&self $(, $param0: $tp0 $(, $param: $tp)*)?) -> $rt {
             self.$subm.$fname($($param0 $(, $param)*)?)
         }
@@ -50,48 +50,48 @@ macro_rules! delegate {
 }
 
 macro_rules! delegate_priv {
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) -> Result) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) -> Result) => {
         fn $fname(self $(, $param0: $tp0 $(, $param: $tp)*)?) -> Result<AppModel, anyhow::Error> {
-            Ok(Model {
+            Ok(AppModel {
                 $subm: self.$subm.$fname($($param0 $(, $param)*)?)?,
                 ..self
             })
         }
     };
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) -> Model) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) -> Model) => {
         fn $fname(self $(, $param0: $tp0 $(, $param: $tp)*)?) -> AppModel {
-            Model {
+            AppModel {
                 $subm: self.$subm.$fname($($param0 $(, $param)*)?),
                 ..self
             }
         }
     };
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) -> $rt:ty) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) -> $rt:ty) => {
         fn $fname(&self $(, $param0: $tp0 $(, $param: $tp)*)?) -> $rt {
             self.$subm.$fname($($param0 $(, $param)*)?)
         }
     };
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) as $name:ident -> Result) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) as $name:ident -> Result) => {
         fn $name(self $(, $param0: $tp0 $(, $param: $tp)*)?) -> Result<AppModel, anyhow::Error> {
-            Ok(Model {
+            Ok(AppModel {
                 $subm: self.$subm.$fname($($param0 $(, $param)*)?)?,
                 ..self
             })
         }
     };
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) as $name:ident -> Model) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) as $name:ident -> Model) => {
         fn $name(self $(, $param0: $tp0 $(, $param: $tp)*)?) -> AppModel {
-            Model {
+            AppModel {
                 $subm: self.$subm.$fname($($param0 $(, $param)*)?),
                 ..self
             }
         }
     };
-    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ident $(, $param:ident: $tp:ty)*)?) as $name:ident -> $rt:ty) => {
+    ($subm:ident, $fname:ident ($($param0:ident: $tp0:ty $(, $param:ident: $tp:ty)*)?) as $name:ident -> $rt:ty) => {
         fn $name(&self $(, $param0: $tp0 $(, $param: $tp)*)?) -> $rt {
             self.$subm.$fname($($param0 $(, $param)*)?)
         }
     };
 }
 
-use delegate;
+pub(in crate::model) use delegate;
